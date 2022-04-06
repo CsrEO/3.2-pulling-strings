4739ad4cf46d13b6e9db3366fe21cf9c9287aa63
Cesar Esquivel <czr.esquivel@gmail.com>
2022-04-05 20:14:35 -0500

    this is solutions for 3.2 pulling strings

---
+++ src/main.js
@@ -0,0 +1,28 @@
+const weather = "weather always changing";
+console.log(weather);
+
+const lower = weather.toLowerCase();
+console.log(lower);
+
+const upper = weather.toUpperCase();
+console.log(upper);
+
+const someDays = weather.replace("weather elways changing");
+console.log(someDays);
+
+const otherDays = weather.replace("weather always changes");
+console.log(otherDays);
+
+const stormOne = "Thunder";
+console.log(stormOne);
+
+const stormTwo = "Lighting";
+console.log(stormTwo);
+
+const stormThree = stormOne + stormTwo;
+console.log(stormThree);
+
+const x = 5;
+const y = 5;
+const sum = `The sum is ${x + y}.`;
+console.log(sum);
